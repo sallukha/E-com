f5b1f24035a07d87127863c845b25abd4b5cee2b
sallu khan <sallukhan54159@gmail.com>
2026-04-25 17:35:48 +0530

    fix buga

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World ka haal chal batana ");
+  res.send("Hello World ka haal chal batana tum ho boka choda ");
 });
-
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
